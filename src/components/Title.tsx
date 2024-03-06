e1f4ae81965709db94655cc83f486e00aa359195
JustifyBaby <quronoa1232@gmail.com>
2024-03-07 00:31:05 +0900

    Public3

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,3 +1,12 @@
-const Title = () => (<div>Title</div>);
+import logo from "../assets/react.svg";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <img src={logo} alt="logo" />
+      <h1>野獣高速交通社（公式）</h1>
+    </div>
+  );
+};
 
 export default Title;
