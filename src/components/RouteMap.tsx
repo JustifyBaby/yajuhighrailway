cbe6611248990d85e70e91ebccad638a8def2186
JustifyBaby <quronoa1232@gmail.com>
2024-03-08 18:04:15 +0900

    Kill Redirects

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -1,17 +1,25 @@
+import { useEffect } from "react";
 import route from "../assets/yaju-route-ver11.pdf";
 
-const RouteMap = () => (
-  <div className="route">
-    <div>
-      <iframe src={route}></iframe>
+
+const RouteMap = () => {
+  useEffect(() => {
+    window.location.href = "/";
+  }, []);
+  return (
+    <div className="route">
+      <div>
+        <iframe src={route}></iframe>
+      </div>
+      <div>
+        <a href={route} target="_blank" rel="noopener">
+          PDF路線図（田所本線、空港線、新幹線）
+        </a>
+      </div>
     </div>
-    <div>
-      <a href={route} target="_blank" rel="noopener">
-        PDF路線図（田所本線、空港線、新幹線）
-      </a>
-    </div>
-  </div>
-);
+  );
+};
+
 
 
 export default RouteMap;
