20cb85d03d8025cbf62ca6559417e5d90ed94df4
JustifyBaby <151998487+JustifyBaby@users.noreply.github.com>
2024-03-08 13:44:55 +0900

    Update RouteMap.tsx

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -1,18 +1,21 @@
 import route from "../assets/yaju-route-ver11.pdf";
 
 const RouteMap = () => {
+  useEffect(()=> {
+    window.location.href = "/";
+  }, []);
   return (
-  <div className="route">
-    <div>
-      <iframe src={route}></iframe>
+    <div className="route">
+      <div>
+        <iframe src={route}></iframe>
+      </div>
+      <div>
+        <a href={route} target="_blank" rel="noopener">
+          PDF路線図（田所本線、空港線、新幹線）
+        </a>
+      </div>
     </div>
-    <div>
-      <a href={route} target="_blank" rel="noopener">
-        PDF路線図（田所本線、空港線、新幹線）
-      </a>
-    </div>
-  </div>
-);
+  );
 };
 
 
